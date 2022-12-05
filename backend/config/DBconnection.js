67e43573732c0171036c9e31e6cea850f949e19b
Anurag Bambardekar <104393371+AnuragBambardekar@users.noreply.github.com>
2022-12-05 13:11:06 -0500

    Latest!
    console.log(err)

--- backend/config/DBconnection.js
+++ backend/config/DBconnection.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
 //Test Connection to Database
 db.connect((err) => {
     if(err){
-        console.log(error)
+        console.log(err)
     }
     else{
         console.log("Connected to Database!")
