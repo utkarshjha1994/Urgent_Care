7192bc4ca09958cc091af589c30e83f1e379ef5b
Anurag Bambardekar <104393371+AnuragBambardekar@users.noreply.github.com>
2022-12-08 10:48:03 -0500

    Latest!
    modified slightly for patient data visibility in viewDocAppt API [Patients password field should not be seen by doctor]

--- backend/api/doctors/doctor.service.js
+++ backend/api/doctors/doctor.service.js
@@ -88,7 +88,7 @@ module.exports = {
         
     },
     viewDocAppointment: (data, callBack) => {
-        db.query('SELECT appointments.*, doctors.doctor_name, doctors.doctor_gender, doctors.doctor_email, doctors.doctor_phone, doctors.doctor_speciality, patients.* FROM appointments INNER JOIN doctors ON appointments.doctor_id=doctors.doctor_id INNER JOIN patients ON appointments.patient_id=patients.patient_id where appointments.doctor_id==?', 
+        db.query('SELECT appointments.*, doctors.doctor_name, doctors.doctor_gender, doctors.doctor_email, doctors.doctor_phone, doctors.doctor_speciality, patients.patient_name, patients.patient_gender, patients.patient_dob, patients.patient_email, patients.patient_phone, patients.patient_address FROM appointments INNER JOIN doctors ON appointments.doctor_id=doctors.doctor_id INNER JOIN patients ON appointments.patient_id=patients.patient_id where appointments.doctor_id=?', 
         [
             data.doctor_id
         ],
