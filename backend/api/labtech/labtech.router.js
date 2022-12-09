33985700a68ceb0834cc9e8c5165958b4ed362bd
Rasika Punde <rasikapunde@gmail.com>
2022-12-09 18:41:54 -0500

    Resolved  node changes

--- backend/api/labtech/labtech.router.js
+++ backend/api/labtech/labtech.router.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validation");
 const { checkUser } = require("../../auth/user_validation");
 
-router.get("/getTests", checkToken, checkUser(["ROLE.LABTECH"]), getTests); 
+router.post("/getTests", checkToken, checkUser(["ROLE.LABTECH"]), getTests); 
 router.put("/modifyTests", checkToken, checkUser(["ROLE.LABTECH"]), modifyTests); 
 router.patch("/changePasswordLabTech", checkToken, checkUser(["ROLE.LABTECH"]),changePassword);
 router.patch("/updateLabTechProfile", checkToken, checkUser(["ROLE.LABTECH"]), updateLabTechProfile); 
